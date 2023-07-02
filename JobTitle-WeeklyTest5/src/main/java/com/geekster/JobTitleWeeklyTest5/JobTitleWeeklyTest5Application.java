5664d25f40d2801d82e5446081cb9e5acfe27896
Ahmedraza6377 <Ahmedraza6377@gmail.com>
2023-07-02 21:47:35 +0530

    job search weekly test -5

---
+++ JobTitle-WeeklyTest5/src/main/java/com/geekster/JobTitleWeeklyTest5/JobTitleWeeklyTest5Application.java
@@ -0,0 +1,13 @@
+package com.geekster.JobTitleWeeklyTest5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTitleWeeklyTest5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTitleWeeklyTest5Application.class, args);
+	}
+
+}
---
+++ JobTitle-WeeklyTest5/src/main/java/com/geekster/JobTitleWeeklyTest5/Model/JobType.java
@@ -0,0 +1,5 @@
+package com.geekster.JobTitleWeeklyTest5.Model;
+
+public enum JobType {
+    IT, HR, Sales, Marketing
+}
